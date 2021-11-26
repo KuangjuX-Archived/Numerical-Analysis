3db15aadc3b19570a13f91d420f5e0c9218db297
KuangjuX <18630816527@163.com>
2021-11-26 16:15:56 +0800

    update

--- draw.py
+++ draw.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def draw_trig(a: float, b: float, c: float, d: float, e: float, f: float):
-    x = np.linspace(a, b, 30)
-    y = c * np.sin(d * x) + e * np.cos(f * x)
-    plt.plot(x, y)
-    plt.show()
+class Drawer:
+    def __init__(self):
+        pass
 
-
-def draw_poly(fn, a: float, b: float):
-    x = np.linspace(a, b, 30)
-    y = fn(x)
-    plt.plot(x, y)
-    plt.show()
+    def draw_interp(self, a: float, b: float, c: float, d: float, e: float, f: float, fn, title: str):
+        x = np.linspace(a, b, 100)
+        y1 = c * np.sin(d * x) + e * np.cos(f * x)
+        y2 = fn(x)
+        fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
+        ax1.plot(x, y1, label = 'True Curve', color = 'red')
+        ax1.legend()
+        ax2.plot(x, y2, label = 'Interp Curve', color = 'green')
+        ax2.legend()
+        fig.suptitle(title)
+        plt.savefig('image/{}.png'.format(title))
+        # plt.show()
--- example.py
+++ example.py
@@ -1,5 +1,6 @@
-from random import sample
+import numpy as np
 import interp
+from draw import Drawer
 from interp.hermite import Hermite
 from interp.lagrange import Largrange
 from interp.newton import Netwon
@@ -7,31 +8,66 @@ from interp.piece_linear import PieceLinear
 from interp.vandermonde import Vandermonde
 
 def test_vandermonde():
-    samples = interp.point_sample(10, 1, 5, 1, 1, 1, 1)
+    npoints = 10
+    a = 0
+    b = 5
+    c = 1
+    d = 1
+    e = 1
+    f = 1
+    samples = interp.point_sample(npoints, a, b, c, d, e, f)
     vandermonde = Vandermonde(samples)
     fn = vandermonde.interp()
-    other_samples = interp.point_sample(10, 1, 5, 1, 1, 1, 1)
+    other_samples = interp.point_sample(npoints, a, b, c, d, e, f)
     interp.point_test(fn, other_samples)
+    drawer = Drawer()
+    drawer.draw_interp(a, b, c, d, e, f, fn, 'Vandermonde Method')
 
 def test_newton():
-    samples = interp.point_sample(10, 1, 5, 1, 1, 1, 1)
+    npoints = 10
+    a = 0
+    b = 5
+    c = 1
+    d = 1
+    e = 1
+    f = 1
+    samples = interp.point_sample(npoints, a, b, c, d, e, f)
     newton = Netwon(samples)
     fn = newton.interp()
-    other_samples = interp.point_sample(10, 1, 5, 1, 1, 1, 1)
+    other_samples = interp.point_sample(npoints, a, b, c, d, e, f)
     interp.point_test(fn, other_samples)
+    drawer = Drawer()
+    drawer.draw_interp(a, b, c, d, e, f, fn, 'Newton Method')
+
 
 def test_lagrange():
-    samples = interp.point_sample(10, 1, 5, 1, 1, 1, 1)
+    npoints = 10
+    a = 0
+    b = 5
+    c = 1
+    d = 1
+    e = 1
+    f = 1
+    samples = interp.point_sample(npoints, a, b, c, d, e, f)
     lagrange = Largrange(samples)
     fn = lagrange.interp()
-    other_samples = interp.point_sample(10, 1, 5, 1, 1, 1, 1)
+    other_samples = interp.point_sample(npoints, a, b, c, d, e, f)
     interp.point_test(fn, other_samples)
+    drawer = Drawer()
+    drawer.draw_interp(a, b, c, d, e, f, fn, 'Lagrange Method')
 
 def test_piecelinear():
-    samples = interp.fixed_sample(1000, 1, 5, 1, 1, 1, 1)
+    npoints = 1000
+    a = 0
+    b = 5
+    c = 1
+    d = 1
+    e = 1
+    f = 1
+    samples = interp.fixed_sample(npoints, a, b, c, d, e, f)
     piece_linear = PieceLinear(samples)
     piece_linear.interp()
-    other_samples = interp.point_sample(10, 1, 5, 1, 1, 1, 1)
+    other_samples = interp.point_sample(npoints, a, b, c, d, e, f)
     for i in range(len(other_samples)):
         y = piece_linear.cal(other_samples[i].x)
         err = abs(y - other_samples[i].y)
@@ -59,11 +95,11 @@ def test_hermite():
 
 
 def example():
-    # test_newton()
-    # test_lagrange()
-    # test_vandermonde()
+    test_newton()
+    test_lagrange()
+    test_vandermonde()
     # test_piecelinear()
-    test_hermite()
+    # test_hermite()
 
 if __name__ == '__main__':
     example()
